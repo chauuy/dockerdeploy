58456ef63cae2e1118906fce382b9b296df942c6
chauuy <64018367+chauuy@users.noreply.github.com>
2020-06-05 19:08:38 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace hello-world
+namespace helloworld
 {
     public class Program
     {
