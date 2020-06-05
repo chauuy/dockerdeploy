5f679c933e05f28406dfa4f87cb357046284ac83
chauuy <64018367+chauuy@users.noreply.github.com>
2020-06-05 17:50:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,32 +1,26 @@
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace PracticalAspNetCore
+namespace aspnetapp
 {
-    public class Startup
-    {
-        public void Configure(IApplicationBuilder app)
-        {
-            app.Run(async context =>
-            {
-                // Duplicate the code below and write more messages. Save and refresh your browser to see the result.
-                await context.Response.WriteAsync("Hello world. Make sure you run this app using 'dotnet watch run'.");
-            });
-        }
-    }
-
     public class Program
     {
-        public static void Main(string[] args) =>
+        public static void Main(string[] args)
+        {
             CreateHostBuilder(args).Build().Run();
+        }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder.UseStartup<Startup>()
-                );
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
