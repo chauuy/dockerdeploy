b66b9b5dde5f2391ff306b29ae0a034394e6d3c2
chauuy <64018367+chauuy@users.noreply.github.com>
2020-06-06 11:58:05 +0200

    Update Index.cshtml.cs

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 
-namespace hellowordl.Pages
+namespace helloworld.Pages
 {
     public class IndexModel : PageModel
     {
