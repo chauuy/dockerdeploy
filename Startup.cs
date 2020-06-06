e2bb832b105d0c6548a8278e03f8888b060c3cc5
chauuy <64018367+chauuy@users.noreply.github.com>
2020-06-06 11:42:02 +0200

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,9 @@ namespace app
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            //services.AddRazorPages();
+            services.AddRazorPages().AddRazorPagesOptions(options => {options.RootDirectory = "/app/Pages";});
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
