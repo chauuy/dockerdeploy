eee7bd94b33bbf061e1c05fe09c75a0305e11ca8
chauuy <64018367+chauuy@users.noreply.github.com>
2020-06-12 16:43:37 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -6,3 +6,6 @@ go
 
 select total_physical_memory_kb/1024 as total_physical_memory_MB,available_physical_memory_kb/1024 as available_physical_memory_MB from sys.dm_os_sys_memory;
 go
+
+#select * from sys.database_files;
+#go
