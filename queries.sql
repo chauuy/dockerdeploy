b9c7ccf587f11ea9c8bf8c38225ec2cbac3e6e1c
chauuy <64018367+chauuy@users.noreply.github.com>
2020-06-12 10:28:41 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 select @@version;
 go
 
-SELECT CONVERT(char(40), SERVERPROPERTY('servername')) ServerName,CONVERT(char(40), SERVERPROPERTY('InstanceName')) instancename,CONVERT(char(20),SERVERPROPERTY('MachineName')) hostname;
+SELECT CONVERT(char(40), SERVERPROPERTY('servername')) ServerName,CONVERT(char(40), SERVERPROPERTY('InstanceName')) instancename,CONVERT(char(40),SERVERPROPERTY('MachineName')) hostname;
 go
 
 select total_physical_memory_kb/1024 as total_physical_memory_MB,available_physical_memory_kb/1024 as available_physical_memory_MB from sys.dm_os_sys_memory;
