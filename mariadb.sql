8f24db5cdea470b5357bd5fe7acb1e9a33f25d89
chauuy <64018367+chauuy@users.noreply.github.com>
2021-07-09 21:22:03 +0200

    Update mariadb.sql

--- mariadb.sql
+++ mariadb.sql
@@ -1,10 +1,14 @@
-CREATE USER 'myuser'@'%' IDENTIFIED BY 'mypass';
+CREATE USER 'myuser'@'%' IDENTIFIED BY 'mypass'; //remote access
 
-GRANT ALL PRIVILEGES ON *.* TO 'myuser'@'%' IDENTIFIED BY 'mypass' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'mariadb'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;
  FLUSH PRIVILEGES;
+
+UPDATE user SET Password=PASSWORD(‘pass’) where USER='mariadb';
 
 SELECT User, Host FROM mysql.user;
 
 show databases;
+
+show tables;
 
 GRANT ALL PRIVILEGES ON database.sampledb TO 'mariadb'@'%';
