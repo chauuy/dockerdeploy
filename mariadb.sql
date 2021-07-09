9b94072f9f25c4089fc6c79e2da2ffb7b59ca59c
chauuy <64018367+chauuy@users.noreply.github.com>
2021-07-09 21:07:05 +0200

    Create mariadb.sql

---
+++ mariadb.sql
@@ -0,0 +1,9 @@
+CREATE USER 'myuser'@'%' IDENTIFIED BY 'mypass';
+
+GRANT ALL PRIVILEGES ON *.* TO 'myuser'@'%' IDENTIFIED BY 'mypass' WITH GRANT OPTION;
+ FLUSH PRIVILEGES;
+
+SELECT User, Host FROM mysql.user;
+
+
+GRANT ALL PRIVILEGES ON database.* TO 'myuser'@'%';
