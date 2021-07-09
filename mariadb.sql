8211a10a9d355a9376650b70821cbbf28bd094be
chauuy <64018367+chauuy@users.noreply.github.com>
2021-07-09 23:37:13 +0200

    Update mariadb.sql

--- mariadb.sql
+++ mariadb.sql
@@ -1,15 +1,22 @@
 mysql -u root
 CREATE DB sampledb;
-CREATE USER 'mariadb'@'%' IDENTIFIED BY 'pass'; //remote access
-CREATE USER 'mariadb'@'localhost' IDENTIFIED BY 'pass';
+CREATE USER 'mariadb'@'%' IDENTIFIED BY 'pass'; //remote access all IPs
+CREATE USER 'mariadb'@'localhost' IDENTIFIED BY 'pass'; //local access
 
-GRANT ALL PRIVILEGES ON *.* TO 'mariadb'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'mariadb'@'%' IDENTIFIED BY 'pass' WITH GRANT OPTION; //pour toutes les DBs
 GRANT ALL PRIVILEGES ON *.* TO 'mariadb'@'localhost' IDENTIFIED BY 'pass' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+
+GRANT ALL PRIVILEGES ON database.sampledb TO 'mariadb'@'%'; //pour 1 DB specifique
 
 USE sampledb;
 CREATE OR REPLACE TABLE t1(c1 int,c2 char(10));
 
+INSERT INTO t1 VALUES(1,"item 1");
+INSERT INTO t1 VALUES(2,"item 2");
+INSERT INTO t1 VALUES(3,"item 3");
+--
 UPDATE user SET Password=PASSWORD(‘pass’) where USER='mariadb';
 
 SELECT User, Host FROM mysql.user;
@@ -17,6 +24,5 @@ SELECT User, Host FROM mysql.user;
 show databases;
 show tables;
 
-GRANT ALL PRIVILEGES ON database.sampledb TO 'mariadb'@'%';
 
 mysql -u mariadb -ppass -h localhost sampledb
